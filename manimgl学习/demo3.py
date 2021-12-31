fba677fc90d033eef6f2f4b4b443c4800ba68c4b
Quinn <quinn20160408@gmail.com>
2021-12-31 16:15:05 +0800

    Add files via upload

---
+++ manimgl学习/demo3.py
@@ -0,0 +1,41 @@
+from manimlib import *
+
+class TextExample(Scene):
+    def construct(self):
+        # 想要正确运行这个场景，你需要确保你的计算机中安装了Consolas字体
+        # 关于Text全部用法，请见https://github.com/3b1b/manim/pull/680
+        text = Text("Here is a text", font="Consolas", font_size=90)
+        difference = Text(
+            """
+            The most important difference between Text and TexText is that\n
+            you can change the font more easily, but can't use the LaTeX grammar
+            """,
+            font="Arial", font_size=24,
+            # t2c是一个由 文本-颜色 键值对组成的字典
+            t2c={"Text": BLUE, "TexText": BLUE, "LaTeX": ORANGE}
+        )
+        VGroup(text, difference).arrange(DOWN, buff=1)
+        self.play(Write(text))
+        self.play(FadeIn(difference, UP))
+        self.wait(3)
+
+        fonts = Text(
+            "And you can also set the font according to different words",
+            font="Arial",
+            t2f={"font": "Consolas", "words": "Consolas"},
+            t2c={"font": BLUE, "words": GREEN}
+        )
+        fonts.set_width(FRAME_WIDTH - 1)
+        slant = Text(
+            "And the same as slant and weight",
+            font="Consolas",
+            t2s={"slant": ITALIC},
+            t2w={"weight": BOLD},
+            t2c={"slant": ORANGE, "weight": RED}
+        )
+        VGroup(fonts, slant).arrange(DOWN, buff=0.8)
+        self.play(FadeOut(text), FadeOut(difference, shift=DOWN))
+        self.play(Write(fonts))
+        self.wait()
+        self.play(Write(slant))
+        self.wait()
